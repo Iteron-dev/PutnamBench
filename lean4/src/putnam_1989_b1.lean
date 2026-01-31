c261db79c8a5fe38a29bda9939ae2e2caef1fe1e
Moritz Firsching <firsching@google.com>
2026-01-31 10:48:28 +0100

    fix build

--- lean4/src/putnam_1989_b1.lean
+++ lean4/src/putnam_1989_b1.lean
@@ -12,7 +12,7 @@ theorem putnam_1989_b1
     (center : EuclideanSpace ℝ (Fin 2))
     (square_def : square = {p | ∀ i : Fin 2, p i ∈ Set.Icc 0 1})
     (perimeter_def : perimeter = {p ∈ square | p 0 = 0 ∨ p 0 = 1 ∨ p 1 = 0 ∨ p 1 = 1})
-    (center_def : center = toLp (fun i : Fin 2 => (1 : ℝ) / 2))
+    (center_def : center =  WithLp.toLp 2 (fun i : Fin 2 => 1 / 2))
     (hScloser : Scloser = {p ∈ square | ∀ q ∈ perimeter, dist p center < dist p q}) :
     let (a, b, c, d) := putnam_1989_b1_solution;
     b > 0 ∧ d > 0 ∧ (¬∃ n : ℤ, n^2 = b) ∧
